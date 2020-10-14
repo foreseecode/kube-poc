b76302fddeda6686fa1d7378c5f54ea562cd8a70
Jing Xiao <jing.xiao@verint.com>
2020-10-14 12:18:23 -0700

    change helm strucutre to be app based

--- dev-qal/version_replace.sh
+++ dev-qal/version_replace.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 function version_replace {
-  if [ "$#" -lt 3 ] ; then echo $0 '<file_name> <app_name> <version>'; return 99; fi
-  _TARGET=$1
-  _APP=$2
+  if [ "$#" -lt 3 ] ; then echo $0 '<env> <app_name> <version>'; return 99; fi
+  _TARGET="$1/$2/version.conf"
   _VER=$3
-  _TMP=__$$.txt
-  cat $_TARGET | tr '\n' '\r' | sed -e "s|  $_APP\:\r    version\: \"snapshot[-0-9]*\"|  $_APP\:\r    version\: \"$_VER\"|g" | tr '\r' '\n' > $_TMP
-  rm $_TARGET
-  mv $_TMP $_TARGET
+  echo -n $_VER > $_TARGET
 }
 version_replace $*
