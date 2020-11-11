4cb65f797d2d59a26fc82d803e677eeea74909ce
Jing Xiao <jing.xiao@verint.com>
2020-11-10 23:16:28 -0800

    change the ingress, restructure deployment

--- dev-qal/version_replace.sh
+++ dev-qal/version_replace.sh
@@ -4,6 +4,7 @@ function version_replace {
   if [ "$#" -lt 3 ] ; then echo $0 '<env> <app_name> <version>'; return 99; fi
   _TARGET="$1/$2/version.conf"
   _VER=$3
+  mkdir -p $1/$2
   echo -n $_VER > $_TARGET
 }
 version_replace $*
