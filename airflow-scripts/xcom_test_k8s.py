6cd0b966d30fb039dc981f23249c35bfdf4b1b6b
Pratik Zingade <pratik.zingade@foresee.com>
2022-05-20 10:54:36 -0700

    Adding daily

--- airflow-scripts/xcom_test_k8s.py
+++ airflow-scripts/xcom_test_k8s.py
@@ -17,7 +17,7 @@ default_args = {
 }
 
 dag = DAG(
-    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    'kubernetes_sample', default_args=default_args, schedule_interval='@daily')
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
